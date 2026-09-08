6913ecb5a75c30c4f7a003e4377f70231bdf7229
sezeranomoise <aladdinmoise@gmail.com>
2026-09-08 09:12:42 +0200

    Add Movie.js with initial movie data

---
+++ Movie.js
@@ -0,0 +1,16 @@
+const movies = [
+  {
+    title: "Movie One",
+    year: 2026,
+    genre: "Action",
+    poster: "",
+    video: ""
+  },
+  {
+    title: "Movie Two",
+    year: 2026,
+    genre: "Drama",
+    poster: "",
+    video: ""
+  }
+];
